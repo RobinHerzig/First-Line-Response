e88508a7965e8f931c3d8f989007f36dc18ffcbd
RobinHerzig <robin.hrzg@gmail.com>
2022-10-25 23:43:12 -0500

    Tailwind and DasiyUI  optimization

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const logger = require('morgan')
 const connectDB = require('./config/database')
 const homeRoutes = require('./routes/home')
 const demoRoutes = require('./routes/demo')
-// const MongoClient = require('mongodb').MongoClient
-// dateTime = require('node-datetime')
 const PORT = 8000
 
 // .ENV config
@@ -64,99 +62,3 @@ app.use('/demo', demoRoutes)
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running, access via 'localhost:${PORT}'.`)
 })
-
-
-///////////////////////////////////////////////////
-
-
-// let db,
-//     dbConnectionStr = process.env.DB_STRING,
-//     dbName = 'CAD'
-
-// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-//     .then(client => {
-//         console.log(`Connected to ${dbName} Database`)
-//         db = client.db(dbName)
-//     })
-    
-
-// Load homepage
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/views/index.html')
-// })
-
-// // Load CAD
-
-// app.get('/cad',(req, res)=>{
-//     db.collection('calls').find().toArray()
-//     .then(data => {
-//         res.render('cad.ejs', { info: data })
-//     })
-//     .catch(err => console.log(err))
-// })
-
-// // Create new call
-
-// app.post('/createCall', (req, res) => {
-//     const dt = dateTime.create()
-//     const date = dt.format('Y-m-d')
-//     const time = dt.format('H:M:S')
-
-//     db.collection('calls').insertOne({
-//         date: date,
-//         time: time,
-//     })
-//     .then(data => {
-//         console.log('Created new call')
-//         res.json(data)
-//     })
-//     .catch(err => console.log(err))
-// })
-
-// // Display selected call
-
-// app.get('/displaySelectedCall', (req, res) => {
-//     db.collection('calls').find().toArray()
-//     .then(data => {
-//         console.log('Displayed selected call')
-//         res.json(data)
-//     })
-//     .catch(err => console.log(err))
-// })
-
-// // Save selected call
-
-// app.put('/saveSelectedCall', (req, res) => {
-//     const callInfoDataObject = {}
-//     const dt = dateTime.create()
-//     const date = dt.format('Y-m-d')
-//     const time = dt.format('H:M:S')
-//     const timeStamp = date + ' ' + time
-//     for (key in req.body) {
-//         if (key === 'newNote' && req.body[key] !== '') {
-//             callInfoDataObject[`callNotesObject.${timeStamp}`] = time + ': ' + [req.body[key]] // Create embedded document for call notes
-//         }
-//         else {
-//             callInfoDataObject[key] = req.body[key] // Iterate through the request body, create an object out of key/value pairs
-//         }
-//     }
-//     db.collection('calls').updateOne({ "_id": ObjectId(req.body.id)}, {$set: callInfoDataObject})
-//     .then(data => {
-//         console.log(callInfoDataObject)
-//         console.log('Saved selected call')
-//         res.json('Saved selected call')
-//     })
-//     .catch(err => console.log(err))
-// })
-
-// // Delete selected call
-
-// app.delete('/deleteSelectedCall', (req, res) => {
-//     db.collection('calls').deleteOne({ "_id": ObjectId(req.body.id)})
-//     .then(data => {
-//         console.log('Deleted selected call')
-//         res.json('Deleted selected call')
-//     })
-//     .catch(err => console.log(err))
-// })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ["./views/**/*.ejs"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ["./views/**/*.ejs"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#075985",
+          "secondary": "#0284c7",
+          "accent": "#5b21b6",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
